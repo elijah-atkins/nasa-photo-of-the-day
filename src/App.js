0e628a795db353e2674f2e4ccd756413ff17a474
Elijah Atkins <elijahorama@gmail.com>
2020-04-14 22:09:57 -0600

    added nasa logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import PictureGet from "./components/PictureGet"
+import Logo from "./img/NASA_logo.svg"
 
 function App() {
   return (
     <div className="App">
       <div className="header logo-container">
-        <h1>Nasa Picture of the Day</h1>
+        <div className="logo">
+          <img alt="NASA Logo" src={Logo}></img>
+        </div>
+        <div className="logo-header">
+        <h1>Astronomy Picture of the Day</h1>
+        </div>
       </div>
       <PictureGet />
     </div>
