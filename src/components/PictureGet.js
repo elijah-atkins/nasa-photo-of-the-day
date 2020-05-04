48bd783fd01b897f00dd7de3538dc5ce1a089850
elijah-atkins <elijahorama@gmail.com>
2020-05-04 17:06:36 -0600

    adding react-router-dom support

--- src/components/PictureGet.js
+++ src/components/PictureGet.js
@@ -3,22 +3,31 @@ import axios from "axios";
 import PictureCard from "./PictureCard"
 import Calendar from 'react-calendar';
 import log from '../img/NASA_logo_alt.svg'
+import { useHistory } from 'react-router-dom'
 
 
 const PictureGet = (props) => {
-
+    const history = useHistory();
+    const dateToString = day =>{
+        var y = day.getFullYear().toString();
+        var m = (day.getMonth() + 1).toString();
+        var d = day.getDate().toString();
+        (d.length === 1) && (d = '0' + d);
+        (m.length === 1) && (m = '0' + m);
+        return `${y}-${m}-${d}`;    }
     const [pic, setPic] = useState([]);
     const [date, setDate] = useState(new Date())
-    const dateToString = day =>{
-        return `${day.getFullYear()}-${day.getMonth()+1}-${day.getDate()}`
-    }
+
     const dayValue = day =>{
+        const newDate = dateToString(day)
+        history.push(`/${newDate}`)
         setDate(day)
 }
     useEffect(() => {
         axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=p4C0QgA2Ec5R5E8oVlU5rXuLbdexPLk59gypoEGM&date=${dateToString(date)}`)
         .then(response => {
+            console.log(response.data)
             setPic(response.data)
         })
         .catch(error => alert(`${error}, Please try again later, or try selecting a different day`))
