17fb01dd591e9ef3e833d953e6c13049d1bea703
Elijah-Atkins <elijahorama@gmail.com>
2020-11-13 08:56:33 -0700

    splash page

--- src/components/PictureGet.js
+++ src/components/PictureGet.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import axios from "axios";
 import PictureCard from "./PictureCard";
 import Calendar from "react-calendar";
